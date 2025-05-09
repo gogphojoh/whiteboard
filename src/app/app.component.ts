ef161d7f923ba74fca24ce7db99a27042c6573f0
hector <gogphojoh1@gmail.com>
2025-05-08 18:39:03 -0600

    Revert "Trello-clone"
    This reverts commit 00a5417fbe7f5e41022fd6e197986f2eb6d6c053.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,12 @@
 import { Component } from '@angular/core';
-import { WhiteboardComponent } from './whiteboard/whiteboard.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [WhiteboardComponent],
-  template: `<app-whiteboard></app-whiteboard>`,
-  styles: [`
-    :host {
-      display: block;
-      height: 100vh;
-      width: 100vw;
-    }
-  `]
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Whiteboard App';
+  title = 'whiteboard';
 }
